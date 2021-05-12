4905d5565a517afc02fbc90d58a4a69fc42d387d
Shoter99 <69056247+Shoter99@users.noreply.github.com>
2021-05-12 08:02:11 +0200

    Added connection to firebase

--- CrashCourse/crash-course/src/App.js
+++ CrashCourse/crash-course/src/App.js
@@ -1,6 +1,9 @@
+import firebase from './firebase'
 import Header from './componets/Header'
 
+
 function App() {
+  const firebaseApp = firebase.apps[0]
   return (
     <div className="container">
       <Header />
